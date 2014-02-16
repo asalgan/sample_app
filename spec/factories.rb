6cf92d0361a06fdeb8e913ae40337724afd1b735
Alan Salganik <asalganik1@gmail.com>
2014-02-16 13:32:41 -0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name "Alan Salganik"
-		email "asalganik@hotmail.com"
-		password "monkey"
-		password_confirmation "monkey"
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -57,4 +57,20 @@ describe "Static pages" do
     click_link "sample app"
     expect(page).to_not have_selector 'title', text: full_title(' | Home')
   end
+
+  describe "edit" do
+    let(:user) {FactoryGirl.create(:user)}
+    before {visit edit_user_path(user)}
+
+    describe "page" do
+      it {should have_content("Update your profile")}
+      it {should have_title("Edit user")}
+      it {should have_link('change', href: 'http://gravatar.com/emails')}
+    end
+
+    describe "with invalid information" do
+      before {click_button "Save changes"}
+      it {should have_content('error')}
+    end
+  end
 end
